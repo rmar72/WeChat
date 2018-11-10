3b261facc273d85c0647ed75b823067b27a24f71
rsol7 <rsolm72@gmail.com>
2018-11-10 03:04:03 -0700

    Messages Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-
 import './App.css';
+
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
 
 class App extends Component {
   render() {
