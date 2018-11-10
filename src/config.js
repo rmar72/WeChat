65e20d13212f1aeea86b56f473c69ddabb0cfbd8
Ruben Solorio <rsolm72@gmail.com>
2018-11-10 04:18:57 -0700

    Delete config.js

--- src/config.js
+++
@@ -1,4 +0,0 @@
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/04c3f783-6811-4c8d-8451-9f87a9db385f/token";
-const instanceLocator = "v1:us1:04c3f783-6811-4c8d-8451-9f87a9db385f";
-
-export { tokenUrl, instanceLocator }
